517b7a3be0c781ff6189832cf4ab1c8c19541c3b
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-11-15 11:40:48 -0300

    find file preparatorio2.csv

--- src/convert.cpp
+++ src/convert.cpp
@@ -2,4 +2,26 @@
 // Nome: Aaron Martins Leão Ferreira - 202120496 
 // Nome: Wildes Augusto de Sousa  - 202020602
 
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
 
+using namespace std;
+
+// Define Function to readfile 
+
+void read_csv(){
+  ofstream readFile("preparatorio2.csv");
+
+  if (readFile){
+    cout << "[OK] Arquivo encontrado com sucesso" << endl;
+  } else {
+    cout << "[FAILED] Arquivo não encontrado" << endl;
+  }
+
+}
+
+int main(){
+  read_csv();
+}
