73374c1132c89fb179ec8f66ae3a2ad63b44ff93
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-12-03 12:00:32 -0300

    feat: tratando o arquivo aux

--- src/convert.cpp
+++ src/convert.cpp
@@ -1,14 +1,44 @@
-#include "cstring"
-#include "iostream"
-#include "fstream"
+#include <cstring>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
-void kitwrite(string aux){
-    ofstream arquivoSaida("saida.bin", ios::in| ios::out| ios::binary | ios::app);
-    if (arquivoSaida){
-        arquivoSaida.write((aux.c_str()),sizeof(aux));
+bool Maisculo(char aux){
+    if (aux >= 'A' and aux <= 'Z'){
+        return true;
+    }
+    return false;
+}
+
+string auxTreatment(string aux){
+    unsigned long tamAux = aux.size();
+
+    for (unsigned long i = 0; i < tamAux; i++){
+        if (aux[i] == ','){
+            aux[i] = ';';
+        }
+
+        if ((aux[i] == ' ') and (aux[(i + 1)] == ' ')){
+            aux[i] = aux[(i - 1)];
+        }
         
+        if ((aux[i] == ' ') and (aux[(i + 1)]) == Maisculo(aux[(i + 1)])){
+            aux[i] = '\n';
+        }
+    }
+    
+    aux = (aux + '\n');
+
+    return aux;
+}
+
+void kitwrite(string aux){
+   /* ofstream arquivoSaida("saida.bin", ios::in| ios::out| ios::binary | ios::app); */
+    ofstream arquivoSaida("saida.txt", ios::in | ios::out | ios::app);
+    if (arquivoSaida){
+        //arquivoSaida.write((aux.c_str()),sizeof(aux));
+        arquivoSaida << auxTreatment(aux);
     }
     arquivoSaida.close();
 }
@@ -20,11 +50,11 @@ void dd_csv2bin(){
     ifstream arquivoLeitura("preparatorio2.csv");
 
     if (arquivoLeitura){
-        
-        while (arquivoLeitura >> aux){
-            kitwrite(aux);
-        };
-        
+        while (getline(arquivoLeitura, aux)){
+            if ((aux[0] >= 'A') and (aux[0] <= 'Z')){
+                kitwrite(aux);
+            }
+        }
         arquivoLeitura.close();
     }
 }
