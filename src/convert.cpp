c657fa7188609168723351de3cc01b4b064f6d58
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-12-03 17:46:53 -0300

    uncompleted src

--- src/convert.cpp
+++ src/convert.cpp
@@ -1,20 +1,91 @@
 #include <cstring>
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 
 using namespace std;
+/*
+class Node{
+    friend class Fila;
+    private:
+        string Nome;
+        string Graduacao;
+        string DataEntrada;
+        string DataSaida;
+        float NumeroMatricula;
+        float QuantiaGasta;
+        Node* next;
+    public:
+        Node(string rNome, string rGraduacao, string rDataEntrada, string rDataSaida, float rNumeroMatricula, float rQuantiaGasta){
+        Nome = rNome;
+        Graduacao = rGraduacao;
+        DataEntrada = rDataEntrada;
+        DataSaida = rDataSaida;
+        NumeroMatricula = rNumeroMatricula;
+        QuantiaGasta = rQuantiaGasta;
+    }
+};
 
+class Fila{
+    friend class Node;
+    private:
+        int Tamanho;
+        Node* inicio;
+        Node* fim;
+    public:
+        Fila(){
+            Tamanho = 0;
+            inicio = NULL;
+            fim = NULL;
+        };
+
+        ~Fila(){
+            while(Tamanho > 0){
+                desenfileira();
+            }
+        }
+        void enfileira(string Nome, string Graduacao, string DataEntrada, string DataSaida, float NumeroMatricula, float QuantiaGasta){
+        Node* novo = new Node(Nome, Graduacao, DataEntrada, DataSaida, NumeroMatricula, QuantiaGasta);
+
+        if (Tamanho == 0){
+            inicio = novo;
+        }else{
+            fim->next = novo;
+        }
+        novo->next = fim;
+        fim = novo;
+        Tamanho ++;
+        }
+
+        Node desenfileira(){
+            Node valor = *inicio;
+            Node* temp = inicio;
+            inicio = inicio->next;
+            delete temp;
+            Tamanho --;
+            if (Tamanho == 0){
+                fim = NULL;
+            }
+            return valor;
+        }
+};
+*/
+string TratamentoAux(string aux){
+    return aux;
+}
 
 void dd_csv2bin(){
     string aux;
-    ifstream arquivoLeitura("preparatorio2.csv");
-    if (arquivoLeitura){
-        while (getline(arquivoLeitura >> ws, aux)){
+    
+    ifstream Arquivoleitura("preparatorio2.csv");
+    if (Arquivoleitura){
+        while (getline(Arquivoleitura, aux)) {
+            cout << aux << endl;
+            cout << TratamentoAux(aux) << endl;
         }
     }
 }
 
 int main(){
-    dd_csv2bin();
     return 0;
-};
+}
--- src/edicao_binario.cpp
+++ src/edicao_binario.cpp
@@ -4,35 +4,29 @@
 
 using namespace std;
 
-void imprimir(string aux){
-    ofstream arquivoSaida("saida.txt", ios::in| ios::out| ios::binary | ios::app);
-    if (arquivoSaida){
-        arquivoSaida.read((aux.c_str()),sizeof(aux));
-        
-    }
-    arquivoSaida.close();
-}
+
 
 void Menu() {
   int option = 0;
-  while (option != 5) {
+  while (option != 6) {
 	  cout << "Digite um Número de 1 a 5."<< endl;
 	  cout << "Opção 1: Adicionar um elemento."<< endl;
 	  cout << "Opção 2: Ver registros entre x e y."<< endl;
 	  cout << "Opção 3: Alterar dados de um Registro."<< endl;
 	  cout << "Opção 4: Trocar dois registros de posição."<< endl;
 	  cout << "Opção 5: Imprimir todos os registros ."<< endl;
-  
-  cin>>option;
-  switch(option){
-	  case 1
+      cout << "Opção 6: Sair do programa ." << endl;
+    cin>>option;
+    switch(option){
+        case 1:
+            break;
 
+        }
+    }
 }
-}
+
 int main(){
-    
-    
-    
+    Menu();
     return 0;
 }
 
