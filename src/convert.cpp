0e2dd399950dcf10a3824b12464304564bbe1d1c
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-12-04 21:38:07 -0300

    adding comments in convert.cpp

--- src/convert.cpp
+++ src/convert.cpp
@@ -9,6 +9,8 @@
 
 using namespace std;
 
+// Criamos uma estrutura utilizando orientação a objetos para servir de fila dos dados a serem escritos
+
 class Node{
     friend class Fila;
     private:
@@ -71,21 +73,32 @@ class Fila{
 };
 
 void dd_csv2bin(class Fila ppufla){
+
+    // Declaramos o arquivo de leitura como preparatorio2.csv e caso o programa identifique que o arquivo existe ele começa a ler linha a linha do arquivo e o enfileira na nossa estrutura "FILA"
+
     ifstream arquivoLeitura("preparatorio2.csv");
  
     string aux;
 
     if (arquivoLeitura){
         while(getline(arquivoLeitura, aux)){
-            cout << "enfileirando: " << aux << endl;
             ppufla.enfileira(aux);
         }
     }
     arquivoLeitura.close();
-    
+ 
+    // Após fechar o arquivo de leitura e ter enfileirado todas as linhas, abrimos um arquivo de escrita que será escrito de forma binária
+
     ofstream arquivoEscrita("saida.bin",ios::in|ios::out|ios::binary|ios::app);
 
     if (arquivoEscrita){
+        
+        // Se o arquivo estiver aberto
+        
+        // Enquanto a fila não estiver vazia desenfileiramos o arquivo em uma strnig auxilar e capturamos seu tamanho. Em seguida geramos um vetor de mesmo tamanho da string e copiamos o valor da string para o vetor de char.
+        
+        // Completando isso escrevemos o vetor de char no arquivo binário 
+
         while (!ppufla.isEmpty()){
             aux = ppufla.desenfileira();
 
@@ -97,11 +110,13 @@ void dd_csv2bin(class Fila ppufla){
             arquivoEscrita.write(auxW, sizeof(auxW));
         }
     }
-
+    
+    // Após terminar o arquivo de escrita é fechado com sucesso
     arquivoEscrita.close();
 }
 
 int main(){
     Fila ppufla;
     dd_csv2bin(ppufla);
+    return 0;
 }
