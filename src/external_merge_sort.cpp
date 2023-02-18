699e686030a6a69dfcac1d600a261bea8a0dc410
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-02-18 18:59:27 -0300

    adding external merge sort

---
+++ src/external_merge_sort.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+#include "fstream"
+#include "auxiliar.hpp"
+
+#define tamanho_arqin 292864;
+#define QTD_PARTICOES 256;
+#define TAMANHO_PARTICAO tamanho / qtd_particoes
+
+using namespace std;
+
+int main(){
+  
+  string arq_in = "saida.bin";
+  string arq_out = "saida_org.bin";
+
+  return 0;
+}
