4ed1442c28ab216b2b88d948d292f3ba95ee2a7a
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-02-19 02:08:58 -0300

    initial merge sort

--- src/external_merge_sort.cpp
+++ src/external_merge_sort.cpp
@@ -1,17 +1,44 @@
-#include "iostream"
-#include "fstream"
-#include "auxiliar.hpp"
-
-#define tamanho_arqin 292864;
-#define QTD_PARTICOES 256;
-#define TAMANHO_PARTICAO tamanho / qtd_particoes
-
+#include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
 
-int main(){
-  
-  string arq_in = "saida.bin";
-  string arq_out = "saida_org.bin";
+#define INT_MAX 1024
 
-  return 0;
+void intercala(int a[], int inicio, int meio, int fim){
+  int i = inicio, j = meio + 1, tamanho = fim - inicio + 1;
+  int aux[tamanho];
+
+  for (int k = 0; k < tamanho; k++){
+    if ((i <= meio ) and (j <= fim)){
+      if (a[i] <= a[j]){
+        aux[k] = a[i];
+        i++;
+      }else{
+        aux[k] = a[j];
+        j++;
+      }
+    }else if (i > meio){
+      aux[k] = a[j];
+      j++;
+    }else{
+      aux[k] = a[i];
+      i++;
+    }
+  }
+  
+  for (int k = 0; k < tamanho; k++){
+    a[inicio + k] = aux[k];
+  }
+}
+
+void mergesort(int a[], int inicio, int fim){
+  int meio;
+
+  if (inicio < fim){
+    meio = (inicio + fim) / 2;
+    mergesort(a, inicio, meio);
+    mergesort(a, meio + 1, fim);
+    intercala(a, inicio, meio, fim);
+  }
 }
