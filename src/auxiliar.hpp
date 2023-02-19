e2070d542431104718e90129aacf6e4c58c592fc
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-02-18 23:55:27 -0300

    feat: improved source code

--- src/auxiliar.hpp
+++ src/auxiliar.hpp
@@ -1,42 +1,98 @@
-#include <cstring>
-#define MAX_VALUE 150
+#ifndef auxiliar_hpp
 
-#include "iostream"
-#include "cstring"
-#include "string"
+#define auxiliar_hpp
+
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+
+#define max_value 150
 
 using namespace std;
 
 struct data_auxiliar{
-  char Series_reference[MAX_VALUE];
-  char Period[MAX_VALUE];
-  float Data_value;
-  char Status[MAX_VALUE];
-  char Units[MAX_VALUE];
-  int Magnitude;
-  char Subject[MAX_VALUE]; 
-  char Periodicity[MAX_VALUE]; 
-  char Group[MAX_VALUE]; 
-  char Series_title_1[MAX_VALUE]; 
-  char Series_title_2[MAX_VALUE]; 
-  char Series_title_3[MAX_VALUE]; 
-  char Series_title_4[MAX_VALUE]; 
-  char Series_title_5[MAX_VALUE]; 
+  char series_reference[max_value];
+  char period[max_value];
+  float data_value;
+  char status[max_value];
+  char units[max_value];
+  int magnitude;
+  char subject[max_value]; 
+  char periodicity[max_value]; 
+  char group[max_value]; 
+  char series_title_1[max_value]; 
+  char series_title_2[max_value]; 
+  char series_title_3[max_value]; 
+  char series_title_4[max_value]; 
+  char series_title_5[max_value]; 
 };
 
-void rw_dados(string str_list[], data_auxiliar &aux){
-  strcpy(aux.Series_reference, str_list[0].c_str());
-  strcpy(aux.Period, str_list[1].c_str());
-  aux.Data_value = atof(str_list[2].c_str());
-  strcpy(aux.Status, str_list[3].c_str());
-  strcpy(aux.Units, str_list[4].c_str());
-  aux.Magnitude = stoi(str_list[5]);
-  strcpy(aux.Subject, str_list[6].c_str());
-  strcpy(aux.Periodicity, str_list[7].c_str());
-  strcpy(aux.Group, str_list[8].c_str());
-  strcpy(aux.Series_title_1, str_list[9].c_str());
-  strcpy(aux.Series_title_2, str_list[10].c_str());
-  strcpy(aux.Series_title_3, str_list[11].c_str());
-  strcpy(aux.Series_title_4, str_list[12].c_str());
-  strcpy(aux.Series_title_5, str_list[13].c_str());
+bool rw_dados(string str_list[], data_auxiliar &aux) {
+  if (str_list[0].size() >= sizeof(aux.series_reference)) {
+    return false;
+  }
+  strncpy(aux.series_reference, str_list[0].c_str(), sizeof(aux.series_reference));
+
+  if (str_list[1].size() >= sizeof(aux.period)) {
+    return false;
+  }
+  strncpy(aux.period, str_list[1].c_str(), sizeof(aux.period));
+
+  aux.data_value = atof(str_list[2].c_str());
+
+  if (str_list[3].size() >= sizeof(aux.status)) {
+    return false;
+  }
+  strncpy(aux.status, str_list[3].c_str(), sizeof(aux.status));
+
+  if (str_list[4].size() >= sizeof(aux.units)) {
+    return false;  
+  }
+  strncpy(aux.units, str_list[4].c_str(), sizeof(aux.units));
+
+  aux.magnitude = stoi(str_list[5]);
+
+  if (str_list[6].size() >= sizeof(aux.subject)) {
+    return false;
+  }
+  strncpy(aux.subject, str_list[6].c_str(), sizeof(aux.subject));
+
+  if (str_list[7].size() >= sizeof(aux.periodicity)) {
+    return false;
+  }
+  strncpy(aux.periodicity, str_list[7].c_str(), sizeof(aux.periodicity));
+
+  if (str_list[8].size() >= sizeof(aux.group)) {
+    return false;
+  }
+  strncpy(aux.group, str_list[8].c_str(), sizeof(aux.group));
+
+  if (str_list[9].size() >= sizeof(aux.series_title_1)) {
+    return false;
+  }
+  strncpy(aux.series_title_1, str_list[9].c_str(), sizeof(aux.series_title_1));
+
+  if (str_list[10].size() >= sizeof(aux.series_title_2)) {
+    return false;  }
+  strncpy(aux.series_title_2, str_list[10].c_str(), sizeof(aux.series_title_2));
+  
+  if (str_list[11].size() >= sizeof(aux.series_title_3)) {
+    return false;
+  }
+
+  strncpy(aux.series_title_3, str_list[11].c_str(), sizeof(aux.series_title_3));
+
+  if (str_list[12].size() >= sizeof(aux.series_title_4)) {
+    return false;
+  }
+  strncpy(aux.series_title_4, str_list[12].c_str(), sizeof(aux.series_title_4));
+
+  if (str_list[13].size() >= sizeof(aux.series_title_5)) {
+    return false;
+  }
+  strncpy(aux.series_title_5, str_list[13].c_str(), sizeof(aux.series_title_5));
+
+  return true;
 }
+#endif
--- src/convert.cpp
+++ src/convert.cpp
@@ -14,24 +14,35 @@ int main(){
   string linha, str_list[14], column;
 
   arq_in.open("property-transfer-statistics-march-2022-quarter-csv.csv", ios::in);
-  arq_out.open("saida.bin", ios::in | ios::out | ios::trunc | ios::binary);
-
-  if (arq_in){
-    getline(arq_in, linha); // Começamos a escrever após o cabecalho     
-    while(getline(arq_in, linha) and !arq_in.eof()){
-      i = 0;
-      stringstream s(linha);
-      while (getline(s, column, ',')) {
-        str_list[i] = column;
-        i++;
-      }
-      rw_dados(str_list, aux);
-      arq_out.write(reinterpret_cast<char*>(&aux), sizeof(aux));
-    }
-    arq_in.close();
-    arq_out.close();
-    cout << "Arquivo criado com sucesso" << endl;
-  }else{
-    cout << "Não foi possivel abrir o arquivo" << endl;
+  
+  if (!arq_in){
+    cout << "Não foi possivel abrir o arquivo de entrada" << endl;
+    return 1;
   }
+
+  arq_out.open("saida.bin", ios::in | ios::out | ios::trunc | ios::binary);
+  
+  if (!arq_out){
+    cout << "Não foi possível abrir o arquivo de said" << endl;
+    return 1;
+  }
+  
+  getline(arq_in, linha);
+
+  while (getline(arq_in, linha) && !arq_in.eof()){
+    i = 0;
+    stringstream s(linha);
+    while (getline(s, column, ',')){
+      str_list[i] = column;
+      i++;
+    }
+    rw_dados(str_list, aux);
+    arq_out.write(reinterpret_cast<char*>(&aux), sizeof(aux));
+  }
+
+  arq_in.close();
+  arq_out.close();
+
+  cout << "Arquivo criado com sucesso" << endl;
+  return 0;
 }
